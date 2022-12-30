2e00b79d6d5a6effa6f504c9026354fcc985d5e9
photowey <photowey@gmail.com>
2022-12-30 21:48:06 +0800

    feat: 1.Add ThreadFactoryBuilder shared code

---
+++ popup-bom/popup-parent/popup-component/popup-common/src/main/java/com/photowey/component/common/shared/google/guava/concurrent/ThreadFactoryBuilder.java
@@ -0,0 +1,203 @@
+/*
+ * Copyright © 2022 the original author or authors (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.component.common.shared.google.guava.concurrent;
+
+import java.util.Locale;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.WARNING;
+
+/**
+ * {@code ThreadFactoryBuilder}
+ *
+ * Add static method {@link ThreadFactoryBuilder#namedThreadFactory(String)}
+ *
+ * @author photowey
+ * @date 2022/12/30
+ * @copy from {@code com.google.common.util.concurrent.ThreadFactoryBuilder}
+ * @since 1.0.0
+ */
+public final class ThreadFactoryBuilder {
+
+    private String nameFormat = null;
+    private Boolean daemon = null;
+    private Integer priority = null;
+    private Thread.UncaughtExceptionHandler uncaughtExceptionHandler = null;
+    private ThreadFactory backingThreadFactory = null;
+
+    /**
+     * Fast build {@link ThreadFactory} by nameFormat
+     * <p>
+     * Example:
+     * <pre>
+     *      ThreadFactory threadFactory = ThreadFactoryBuilder.namedThreadFactory("pop-%d");
+     * </pre>
+     *
+     * @param nameFormat nameFormat
+     * @return {@link ThreadFactory}
+     */
+    public static ThreadFactory namedThreadFactory(String nameFormat) {
+        return new ThreadFactoryBuilder().nameFormat(nameFormat).build();
+    }
+
+    public ThreadFactoryBuilder nameFormat(String nameFormat) {
+        // fail fast if the format is bad or null
+        String unused = format(nameFormat, 0);
+        this.nameFormat = nameFormat;
+        return this;
+    }
+
+    public ThreadFactoryBuilder daemon(boolean daemon) {
+        this.daemon = daemon;
+        return this;
+    }
+
+    public ThreadFactoryBuilder priority(int priority) {
+        checkArgument(
+                priority >= Thread.MIN_PRIORITY,
+                "Thread priority (%s) must be >= %s",
+                priority,
+                Thread.MIN_PRIORITY);
+        checkArgument(
+                priority <= Thread.MAX_PRIORITY,
+                "Thread priority (%s) must be <= %s",
+                priority,
+                Thread.MAX_PRIORITY);
+        this.priority = priority;
+        return this;
+    }
+
+    public ThreadFactoryBuilder uncaughtExceptionHandler(
+            Thread.UncaughtExceptionHandler uncaughtExceptionHandler) {
+        this.uncaughtExceptionHandler = checkNotNull(uncaughtExceptionHandler);
+        return this;
+    }
+
+    public ThreadFactoryBuilder threadFactory(ThreadFactory backingThreadFactory) {
+        this.backingThreadFactory = checkNotNull(backingThreadFactory);
+        return this;
+    }
+
+    public ThreadFactory build() {
+        return doBuild(this);
+    }
+
+    private static ThreadFactory doBuild(ThreadFactoryBuilder builder) {
+        final String nameFormat = builder.nameFormat;
+        final Boolean daemon = builder.daemon;
+        final Integer priority = builder.priority;
+        final Thread.UncaughtExceptionHandler uncaughtExceptionHandler = builder.uncaughtExceptionHandler;
+        final ThreadFactory backingThreadFactory =
+                (builder.backingThreadFactory != null)
+                        ? builder.backingThreadFactory
+                        : Executors.defaultThreadFactory();
+        final AtomicLong count = (nameFormat != null) ? new AtomicLong(0) : null;
+        return new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable runnable) {
+                Thread thread = backingThreadFactory.newThread(runnable);
+                if (nameFormat != null) {
+                    thread.setName(format(nameFormat, count.getAndIncrement()));
+                }
+                if (daemon != null) {
+                    thread.setDaemon(daemon);
+                }
+                if (priority != null) {
+                    thread.setPriority(priority);
+                }
+                if (uncaughtExceptionHandler != null) {
+                    thread.setUncaughtExceptionHandler(uncaughtExceptionHandler);
+                }
+                return thread;
+            }
+        };
+    }
+
+    private static String format(String format, Object... args) {
+        return String.format(Locale.ROOT, format, args);
+    }
+
+    private static <T> T checkNotNull(T reference) {
+        if (reference == null) {
+            throw new NullPointerException();
+        }
+        return reference;
+    }
+
+    private static void checkArgument(
+            boolean b, String errorMessageTemplate, int p1, int p2) {
+        if (!b) {
+            throw new IllegalArgumentException(lenientFormat(errorMessageTemplate, p1, p2));
+        }
+    }
+
+    private static String lenientFormat(String template, Object... args) {
+        template = String.valueOf(template); // null -> "null"
+
+        if (args == null) {
+            args = new Object[]{"(Object[])null"};
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                args[i] = lenientToString(args[i]);
+            }
+        }
+
+        // start substituting the arguments into the '%s' placeholders
+        StringBuilder builder = new StringBuilder(template.length() + 16 * args.length);
+        int templateStart = 0;
+        int i = 0;
+        while (i < args.length) {
+            int placeholderStart = template.indexOf("%s", templateStart);
+            if (placeholderStart == -1) {
+                break;
+            }
+            builder.append(template, templateStart, placeholderStart);
+            builder.append(args[i++]);
+            templateStart = placeholderStart + 2;
+        }
+        builder.append(template, templateStart, template.length());
+
+        // if we run out of placeholders, append the extra args in square braces
+        if (i < args.length) {
+            builder.append(" [");
+            builder.append(args[i++]);
+            while (i < args.length) {
+                builder.append(", ");
+                builder.append(args[i++]);
+            }
+            builder.append(']');
+        }
+
+        return builder.toString();
+    }
+
+    private static String lenientToString(Object o) {
+        try {
+            return String.valueOf(o);
+        } catch (Exception e) {
+            // Default toString() behavior - see Object.toString()
+            String objectToString =
+                    o.getClass().getName() + '@' + Integer.toHexString(System.identityHashCode(o));
+            // Logger is created inline with fixed name to avoid forcing Proguard to create another class.
+            Logger.getLogger("com.google.common.base.Strings")
+                    .log(WARNING, "Exception during lenientFormat for " + objectToString, e);
+            return "<" + objectToString + " threw " + e.getClass().getName() + ">";
+        }
+    }
+}
