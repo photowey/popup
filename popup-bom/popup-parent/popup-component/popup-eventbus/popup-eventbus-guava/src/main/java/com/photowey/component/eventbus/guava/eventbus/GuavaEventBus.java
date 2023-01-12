3d855014401203442b1935259bd56261407a66eb
photowey <photowey@gmail.com>
2023-01-12 21:45:40 +0800

    feat: 1.Update GuavaEventBus

--- popup-bom/popup-parent/popup-component/popup-eventbus/popup-eventbus-guava/src/main/java/com/photowey/component/eventbus/guava/eventbus/GuavaEventBus.java
+++ popup-bom/popup-parent/popup-component/popup-eventbus/popup-eventbus-guava/src/main/java/com/photowey/component/eventbus/guava/eventbus/GuavaEventBus.java
@@ -16,7 +16,11 @@
 package com.photowey.component.eventbus.guava.eventbus;
 
 import com.photowey.component.eventbus.guava.event.Event;
+import com.photowey.component.eventbus.guava.factory.NamedThreadFactory;
 import com.photowey.component.eventbus.guava.subscriber.Subscriber;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
 
 /**
  * {@code GuavaEventBus}
@@ -25,17 +29,78 @@ import com.photowey.component.eventbus.guava.subscriber.Subscriber;
  * @date 2023/01/09
  * @since 1.0.0
  */
+@Slf4j
 public class GuavaEventBus implements EventBus {
 
+    protected static final int CORE_THREAD = 1;
+    protected static final int MAX_THREAD = 1;
+    protected static int DEFAULT_CAPACITY = 1024 << 1;
+    protected final com.google.common.eventbus.EventBus eventBus;
+
+    public GuavaEventBus(String identifier) {
+        this(identifier, false);
+    }
+
+    public GuavaEventBus(String identifier, boolean async) {
+        this(identifier, DEFAULT_CAPACITY, async);
+    }
+
+    public GuavaEventBus(String identifier, int capacity, boolean async) {
+        this(identifier, CORE_THREAD, MAX_THREAD, capacity, async);
+    }
+
+    public GuavaEventBus(String identifier, int corePoolSize, int maximumPoolSize, int capacity, boolean async) {
+        ExecutorService eventExecutor = this.eventExecutor(identifier, corePoolSize, maximumPoolSize, capacity);
+        if (async) {
+            this.eventBus = new com.google.common.eventbus.AsyncEventBus(identifier, eventExecutor);
+        } else {
+            this.eventBus = new com.google.common.eventbus.EventBus(identifier);
+        }
+    }
+
+    public GuavaEventBus(String identifier, boolean async, ExecutorService eventExecutor) {
+        if (async) {
+            this.eventBus = new com.google.common.eventbus.AsyncEventBus(identifier, eventExecutor);
+        } else {
+            this.eventBus = new com.google.common.eventbus.EventBus(identifier);
+        }
+    }
+
+    @Override
     public <EVENT, T extends Subscriber<EVENT>> void register(T subscriber) {
-
+        this.eventBus.register(subscriber);
     }
 
+    @Override
     public <EVENT, T extends Subscriber<EVENT>> void unregister(T subscriber) {
-
+        this.eventBus.unregister(subscriber);
     }
 
+    @Override
     public <T extends Event> void post(T event) {
+        this.eventBus.post(event);
+    }
 
+    public ExecutorService eventExecutor(String identifier, int corePoolSize, int maximumPoolSize, int capacity) {
+        String nameFormat = identifier + "-%d";
+        return new ThreadPoolExecutor(
+                corePoolSize,
+                maximumPoolSize,
+                1 << 3L,
+                TimeUnit.SECONDS,
+                // TODO LinkedBlockingDeque?
+                new ArrayBlockingQueue<>(capacity),
+                NamedThreadFactory.createNameFormat(nameFormat),
+                this.rejectedExecutionHandler()
+        );
+    }
+
+    public RejectedExecutionHandler rejectedExecutionHandler() {
+        return (r, executor) -> {
+            log.warn("eventbus executor queue is full, size:[{}], and then caller run this task", executor.getQueue().size());
+            if (!executor.isShutdown()) {
+                r.run();
+            }
+        };
     }
 }
