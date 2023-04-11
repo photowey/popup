a1b305ce88e15ae6ad7d662906a9b344b9b2eacc
photowey <photowey@gmail.com>
2023-04-11 22:53:10 +0800

    feat: 1.Update: move package

--- popup-bom/popup-parent/popup-component/popup-common/src/main/java/com/photowey/component/common/calculator/date/DateTimeCalculator.java
+++ popup-bom/popup-parent/popup-component/popup-common/src/main/java/com/photowey/component/common/calculator/date/DateTimeCalculator.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.photowey.component.common.calculator;
+package com.photowey.component.common.calculator.date;
 
 import com.photowey.component.common.date.DateUtils;
 
