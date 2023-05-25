3894b99049ad1d20ff683d3d5115d600af8767ec
photowey <photowey@gmail.com>
2023-05-26 00:58:14 +0800

    feat: 1.Update: mongo service-ext

--- popup-bom/popup-parent/popup-starter/popup-starter-mongo/popup-starter-mongo-service/src/main/java/com/photowey/popup/starter/mongo/service/ext/MongoServiceExt.java
+++ popup-bom/popup-parent/popup-starter/popup-starter-mongo/popup-starter-mongo-service/src/main/java/com/photowey/popup/starter/mongo/service/ext/MongoServiceExt.java
@@ -15,15 +15,23 @@
  */
 package com.photowey.popup.starter.mongo.service.ext;
 
+import com.photowey.component.common.func.ThreeConsumer;
+import com.photowey.component.common.geodesy.LocalGeodeticCalculator;
+import com.photowey.component.common.number.BigDecimalUtils;
+import com.photowey.component.common.number.NumberConstants;
 import com.photowey.popup.starter.mongo.core.constant.MongoConstants;
 import com.photowey.popup.starter.mongo.core.document.DocumentUpdateCounter;
 import org.bson.Document;
+import org.springframework.data.geo.*;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.NearQuery;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.util.ObjectUtils;
 
+import java.math.BigDecimal;
 import java.util.function.Consumer;
 
 /**
@@ -43,19 +51,71 @@ public interface MongoServiceExt<T, ID> {
         return null;
     }
 
-    default <PK, TARGET> void addToSet(TARGET target, PK pk, String node, Class<T> clazz) {
+    default <PK, TARGET> void addToSet(
+            TARGET target, PK pk,
+            String node, Class<T> clazz) {
+        this.addToSetOps(target, pk, node, (mongoTemplate, query, update) -> {
+            mongoTemplate.updateFirst(query, update, clazz);
+        });
+    }
+
+    default <V, TARGET> void addToSet(
+            TARGET target, String key, V value,
+            String node, Class<T> clazz) {
+        this.addToSetOps(target, key, value, node, (mongoTemplate, query, update) -> {
+            mongoTemplate.updateFirst(query, update, clazz);
+        });
+    }
+
+    default <PK, TARGET> void batchAddToSet(
+            TARGET target, PK pk,
+            String node, Class<T> clazz) {
+        this.addToSetOps(target, pk, node, (mongoTemplate, query, update) -> {
+            mongoTemplate.updateMulti(query, update, clazz);
+        });
+    }
+
+    default <V, TARGET> void batchAddToSet(
+            TARGET target, String key, V value,
+            String node, Class<T> clazz) {
+        this.addToSetOps(target, key, value, node, (mongoTemplate, query, update) -> {
+            mongoTemplate.updateMulti(query, update, clazz);
+        });
+    }
+
+    default <PK, TARGET> void addToSetOps(
+            TARGET target, PK pk,
+            String node, ThreeConsumer<MongoOperations, Query, Update> fx) {
+        this.addToSetOps(target, DEFAULT_PK_ID, this.wrapPk(pk), node, fx);
+    }
+
+    default <V, TARGET> void addToSetOps(
+            TARGET target, String key, V value,
+            String node, ThreeConsumer<MongoOperations, Query, Update> fx) {
         MongoOperations mongoTemplate = this.mongoOperations();
         if (null == mongoTemplate) {
             return;
         }
-        Query query = new Query(Criteria.where(DEFAULT_PK_ID).is(this.wrapPk(pk)));
+        Query query = new Query(Criteria.where(key).is(value));
         Update update = new Update();
         update.addToSet(node, target);
 
-        mongoTemplate.updateFirst(query, update, clazz);
+        fx.accept(mongoTemplate, query, update);
     }
 
     default <PK, TARGET> void pull(TARGET target, PK pk, String node, Class<T> clazz) {
+        this.bullOps(target, pk, node, (mongoTemplate, query, update) -> {
+            mongoTemplate.updateFirst(query, update, clazz);
+        });
+    }
+
+    default <PK, TARGET> void batchPull(TARGET target, PK pk, String node, Class<T> clazz) {
+        this.bullOps(target, pk, node, (mongoTemplate, query, update) -> {
+            mongoTemplate.updateMulti(query, update, clazz);
+        });
+    }
+
+    default <PK, TARGET> void bullOps(TARGET target, PK pk, String node, ThreeConsumer<MongoOperations, Query, Update> fx) {
         MongoOperations mongoTemplate = this.mongoOperations();
         if (null == mongoTemplate) {
             return;
@@ -64,15 +124,39 @@ public interface MongoServiceExt<T, ID> {
         Update update = new Update();
         update.pull(node, target);
 
-        mongoTemplate.updateFirst(query, update, clazz);
+        fx.accept(mongoTemplate, query, update);
     }
 
-    default <PK> void doUpdate(PK pk, Document document, Class<T> clazz) {
-        ID id = this.wrapPk(pk);
-        Query query = this.createQueryById(id);
-        Update update = Update.fromDocument(document);
 
-        this.mongoOperations().updateFirst(query, update, clazz);
+    default void update(Class<T> clazz, Consumer<Criteria> condition, Consumer<Update> updater) {
+        this.doUpdate(condition, updater, (mongoTemplate, query, update) -> {
+            mongoTemplate.updateFirst(query, update, clazz);
+        });
+    }
+
+    default void batchUpdate(Class<T> clazz, Consumer<Criteria> condition, Consumer<Update> updater) {
+        this.doUpdate(condition, updater, (mongoTemplate, query, update) -> {
+            mongoTemplate.updateMulti(query, update, clazz);
+        });
+    }
+
+    default void doUpdate(
+            Consumer<Criteria> condition,
+            Consumer<Update> updater,
+            ThreeConsumer<MongoOperations, Query, Update> fx) {
+        MongoOperations mongoTemplate = this.mongoOperations();
+        if (null == mongoTemplate) {
+            return;
+        }
+
+        Criteria criteria = new Criteria();
+        condition.accept(criteria);
+
+        Query query = new Query(criteria);
+        Update update = new Update();
+        updater.accept(update);
+
+        fx.accept(mongoTemplate, query, update);
     }
 
     default <PK> void logicDelete(PK pk, Class<T> clazz) {
@@ -82,6 +166,27 @@ public interface MongoServiceExt<T, ID> {
         update.set(DEFAULT_DELETED_KEY, this.deleted());
 
         this.mongoOperations().updateFirst(query, update, clazz);
+    }
+
+    // ------------------------------------------------------------------------- geo
+
+    default BigDecimal tryCalculateDistance(ID objectId, Point point, Class<T> clazz) {
+        MongoOperations mongoTemplate = this.mongoOperations();
+        if (null == mongoTemplate) {
+            return BigDecimalUtils.newZeroBigDecimal();
+        }
+
+        Query query = this.createQueryById(objectId);
+        GeoJsonPoint geo = point instanceof GeoJsonPoint ? (GeoJsonPoint) point : new GeoJsonPoint(point);
+        NearQuery nearQuery = NearQuery.near(geo, Metrics.KILOMETERS);
+        nearQuery.query(query);
+        GeoResults<T> results = mongoTemplate.geoNear(nearQuery, clazz);
+
+        for (GeoResult<T> result : results) {
+            return this.formatKm(result.getDistance());
+        }
+
+        return BigDecimalUtils.newZeroBigDecimal();
     }
 
     // ------------------------------------------------------------------------- fx
@@ -96,6 +201,26 @@ public interface MongoServiceExt<T, ID> {
         return true;
     }
 
+    default <V> boolean setIfTrue(boolean expression, V v, Consumer<V> then) {
+        if (expression) {
+            then.accept(v);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    default boolean setIfTrue(boolean expression, Runnable then) {
+        if (expression) {
+            then.run();
+
+            return true;
+        }
+
+        return false;
+    }
+
     default <V> boolean setIfPresent(V v, DocumentUpdateCounter counter, Consumer<V> then) {
         if (ObjectUtils.isEmpty(v)) {
             return false;
@@ -105,6 +230,28 @@ public interface MongoServiceExt<T, ID> {
         counter.increment();
 
         return true;
+    }
+
+    default <V> boolean setIfPresent(boolean expression, V v, DocumentUpdateCounter counter, Consumer<V> then) {
+        if (expression) {
+            then.accept(v);
+            counter.increment();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    default boolean setIfPresent(boolean expression, DocumentUpdateCounter counter, Runnable then) {
+        if (expression) {
+            then.run();
+            counter.increment();
+
+            return true;
+        }
+
+        return false;
     }
 
     default Document toBsonDocument(T t) {
@@ -123,5 +270,33 @@ public interface MongoServiceExt<T, ID> {
 
     default int deleted() {
         return DEFAULT_DELETED_VALUE;
+    }
+
+    default BigDecimal formatKm(Distance distance) {
+        return this.formatKm(distance.getValue());
+    }
+
+    default BigDecimal formatKm(double distance) {
+        return this.formatKm(distance, NumberConstants.FIVE_DECIMAL_POINTS);
+    }
+
+    default BigDecimal formatKm(double distance, String pattern) {
+        return this.kmTom(distance, pattern);
+    }
+
+    default BigDecimal formatM(double distance) {
+        return LocalGeodeticCalculator.getInstance().formatM(distance);
+    }
+
+    default BigDecimal kmTom(double distanceKm, String pattern) {
+        return LocalGeodeticCalculator.getInstance().kmTom(distanceKm, pattern);
+    }
+
+    default BigDecimal formatDistance(double distance, String pattern) {
+        return LocalGeodeticCalculator.getInstance().formatDistance(distance, pattern);
+    }
+
+    default BigDecimal formatDistance(BigDecimal distanceB, String pattern) {
+        return LocalGeodeticCalculator.getInstance().formatDistance(distanceB, pattern);
     }
 }
