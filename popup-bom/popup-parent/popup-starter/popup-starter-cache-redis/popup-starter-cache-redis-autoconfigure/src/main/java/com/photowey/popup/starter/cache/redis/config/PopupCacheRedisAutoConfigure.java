d16d382507f5f20102076d4ae4ff0c19bad64130
photowey <photowey@gmail.com>
2023-12-24 15:00:52 +0800

    feat: Update: 1.Redis config

--- popup-bom/popup-parent/popup-starter/popup-starter-cache-redis/popup-starter-cache-redis-autoconfigure/src/main/java/com/photowey/popup/starter/cache/redis/config/PopupCacheRedisAutoConfigure.java
+++ popup-bom/popup-parent/popup-starter/popup-starter-cache-redis/popup-starter-cache-redis-autoconfigure/src/main/java/com/photowey/popup/starter/cache/redis/config/PopupCacheRedisAutoConfigure.java
@@ -15,19 +15,34 @@
  */
 package com.photowey.popup.starter.cache.redis.config;
 
-import com.photowey.component.sync.lock.Lock;
-import com.photowey.popup.starter.cache.redis.lock.RedisLock;
-import com.photowey.popup.starter.cache.redis.lock.property.RedisLockProperties;
-import org.redisson.Redisson;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.photowey.popup.starter.cache.redis.core.constant.RedisConstants;
+import com.photowey.popup.starter.cache.redis.property.RedisProperties;
+import com.photowey.popup.starter.cache.redis.proxy.DefaultRedisTemplateProxy;
+import com.photowey.popup.starter.cache.redis.template.RedisTemplateProxy;
 import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * {@code PopupCacheRedisAutoConfigure}
@@ -38,25 +53,107 @@ import org.springframework.context.annotation.Bean;
  */
 @ConditionalOnClass(RedissonClient.class)
 @AutoConfiguration(after = RedisAutoConfiguration.class)
-@EnableConfigurationProperties(value = {RedisLockProperties.class})
+@EnableConfigurationProperties(value = {
+        JedisPoolConfigure.class,
+        JedisConfigure.class,
+})
 public class PopupCacheRedisAutoConfigure {
 
+    private static final String CUSTOM_REDIS_TEMPLATE_BEAN_NAME = RedisConstants.CUSTOM_REDIS_TEMPLATE_BEAN_NAME;
+
+    private static final String REDIS_KEY_SERIALIZER_BEAN_NAME = RedisConstants.REDIS_KEY_SERIALIZER_BEAN_NAME;
+    private static final String REDIS_VALUE_SERIALIZER_BEAN_NAME = RedisConstants.REDIS_VALUE_SERIALIZER_BEAN_NAME;
+
+    @Autowired
+    private RedisProperties redisProperties;
+
     @Bean
-    @ConditionalOnMissingBean(Lock.class)
-    @ConditionalOnProperty(name = "spring.redis.lock.redisson.enabled", havingValue = "true", matchIfMissing = false)
-    public Lock redisLock(RedisLockProperties properties) {
-        RedissonClient redisson = this.populateRedissonClient(properties);
-        return new RedisLock(redisson, properties);
+    @ConditionalOnMissingBean
+    public RedisStandaloneConfiguration redisStandaloneConfiguration() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(this.redisProperties.getHost());
+        redisStandaloneConfiguration.setPort(this.redisProperties.getPort());
+        redisStandaloneConfiguration.setDatabase(this.redisProperties.getDatabase());
+        if (this.redisProperties.determinePasswordIsEnabled()) {
+            redisStandaloneConfiguration.setPassword(RedisPassword.of(this.redisProperties.getPassword()));
+        }
+
+        return redisStandaloneConfiguration;
     }
 
-    private RedissonClient populateRedissonClient(RedisLockProperties properties) {
-        Config config = new Config();
-        config.useSingleServer()
-                .setAddress(properties.getAddress())
-                .setDatabase(properties.getDatabase())
-                .setPassword(properties.getPassword())
-                .setTimeout(properties.getTimeout());
+    @Bean
+    @ConditionalOnMissingBean
+    public RedisConnectionFactory connectionFactory(JedisPoolConfig jedisPoolConfig) {
+        RedisStandaloneConfiguration std = this.redisStandaloneConfiguration();
 
-        return Redisson.create(config);
+        JedisClientConfiguration.JedisPoolingClientConfigurationBuilder configurationBuilder =
+                (JedisClientConfiguration.JedisPoolingClientConfigurationBuilder) JedisClientConfiguration.builder();
+        configurationBuilder.poolConfig(jedisPoolConfig);
+        JedisClientConfiguration conf = configurationBuilder.build();
+
+        return new JedisConnectionFactory(std, conf);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        return stringRedisTemplate;
+    }
+
+    @Bean(REDIS_KEY_SERIALIZER_BEAN_NAME)
+    @ConditionalOnMissingBean(name = REDIS_KEY_SERIALIZER_BEAN_NAME)
+    public RedisSerializer<String> redisKeySerializer() {
+        return new StringRedisSerializer();
+    }
+
+    @Bean(REDIS_VALUE_SERIALIZER_BEAN_NAME)
+    @ConditionalOnMissingBean(name = REDIS_VALUE_SERIALIZER_BEAN_NAME)
+    @ConditionalOnExpression("#{T(org.springframework.util.StringUtils).hasText(environment['spring.redis.serializer']) " +
+            "&& environment['spring.redis.serializer'].startsWith('Jackson')}")
+    public RedisSerializer<Object> redisValueJacksonSerializer(ObjectMapper objectMapper) {
+        objectMapper.registerModule(new JavaTimeModule());
+
+        Jackson2JsonRedisSerializer<Object> redisSerializer = new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
+
+        return redisSerializer;
+    }
+
+    @Bean(REDIS_VALUE_SERIALIZER_BEAN_NAME)
+    @ConditionalOnMissingBean(name = REDIS_VALUE_SERIALIZER_BEAN_NAME)
+    @ConditionalOnExpression("#{T(org.springframework.util.StringUtils).hasText(environment['spring.redis.serializer']) " +
+            "&& environment['spring.redis.serializer'].startsWith('Generic')}")
+    public RedisSerializer<Object> redisValueGenericSerializer(ObjectMapper objectMapper) {
+        //objectMapper.registerModule(new JavaTimeModule());
+
+        return new GenericJackson2JsonRedisSerializer();
+    }
+
+    @Bean(CUSTOM_REDIS_TEMPLATE_BEAN_NAME)
+    @ConditionalOnMissingBean(name = CUSTOM_REDIS_TEMPLATE_BEAN_NAME)
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory,
+            @Qualifier(REDIS_KEY_SERIALIZER_BEAN_NAME) RedisSerializer<String> redisKeySerializer,
+            @Qualifier(REDIS_VALUE_SERIALIZER_BEAN_NAME) RedisSerializer<Object> redisValueSerializer) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setKeySerializer(redisKeySerializer);
+        redisTemplate.setHashKeySerializer(redisKeySerializer);
+
+        redisTemplate.setValueSerializer(redisValueSerializer);
+        redisTemplate.setHashValueSerializer(redisValueSerializer);
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(RedisTemplateProxy.class)
+    public RedisTemplateProxy redisTemplateProxy(RedisTemplate<String, Object> redisTemplate, StringRedisTemplate stringRedisTemplate) {
+        return new DefaultRedisTemplateProxy(redisTemplate, stringRedisTemplate);
     }
 }
